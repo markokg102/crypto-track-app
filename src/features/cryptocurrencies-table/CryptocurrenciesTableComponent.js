f2d45a8cc1c1120c3b46ff5a91de340a6a784405
Marko Manojlovic <markokg102@gmail.com>
2018-07-13 23:22:50 +0200

    basic loading limit 50, structure array coupled in commponent

--- src/features/cryptocurrencies-table/CryptocurrenciesTableComponent.js
+++ src/features/cryptocurrencies-table/CryptocurrenciesTableComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-class CryptocurrenciesTableComponent extends React.Component {
-	render() {
-		return (
-			<table style={{ width: '100%' }}>
+
+const ResultTable = (props) => {
+	return (
+		<table style={{ width: '100%' }}>
+			<thead>
 				<tr>
 					<th>Name</th>
 					<th>Short name</th>
@@ -12,12 +13,52 @@ class CryptocurrenciesTableComponent extends React.Component {
 					<th>Ammount you own</th>
 					<th>$ value of your coin</th>
 				</tr>
-				<tr>
+			</thead>
+			<tbody>
+				{props.responseObject.data.map(cryptocurrency => (
+					<tr key={cryptocurrency.id}>
+						<td>{cryptocurrency.name}</td>
+						<td>{cryptocurrency.symbol}</td>
+						<td>{cryptocurrency.quotes.USD.price}</td>
+						<td>{cryptocurrency.quotes.USD.percent_change_24h}</td>
+						<td>0.5</td>
+						<td>{0.5 * cryptocurrency.quotes.USD.price}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
+};
 
-				</tr>
-			</table>
+
+class CryptocurrenciesTableComponent extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			responseObject: null,
+			isLoading: true
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://api.coinmarketcap.com/v2/ticker/?limit=50&structure=array')
+			.then(response => response.json())
+			.then(responseObject => this.setState({ responseObject, isLoading: false }));
+	}
+
+	render() {
+		const isLoadingSuccessfullyData = !this.state.isLoading && this.state.responseObject;
+
+		return (
+			<div>
+				{!isLoadingSuccessfullyData ?
+					(<h2>Loading</h2 >) : (<ResultTable responseObject={this.state.responseObject} />)
+				}
+			</div>
 		);
 	}
 }
+
 
 export default CryptocurrenciesTableComponent;
