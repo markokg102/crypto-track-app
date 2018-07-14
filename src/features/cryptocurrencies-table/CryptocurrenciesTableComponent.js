b6c045ad62bb942bf3e7be12613ddd768aff1e8b
Marko Manojlovic <markokg102@gmail.com>
2018-07-14 11:52:36 +0200

    inmput ammount you own and submit, eslin arrow methods support

--- src/features/cryptocurrencies-table/CryptocurrenciesTableComponent.js
+++ src/features/cryptocurrencies-table/CryptocurrenciesTableComponent.js
@@ -16,12 +16,23 @@ const ResultTable = (props) => {
 			</thead>
 			<tbody>
 				{props.responseObject.data.map(cryptocurrency => (
-					<tr key={cryptocurrency.id}>
+					<tr key={'row' + cryptocurrency.id}>
 						<td>{cryptocurrency.name}</td>
 						<td>{cryptocurrency.symbol}</td>
 						<td>{cryptocurrency.quotes.USD.price}</td>
 						<td>{cryptocurrency.quotes.USD.percent_change_24h}</td>
-						<td>0.5</td>
+						<td>
+							<form onSubmit={(event) => { event.preventDefault(); }}>
+								<input
+									style={{ width: '100%' }}
+									id={cryptocurrency.id}
+									onChange={props.handleChangeAmmountYouOwn}
+									value={cryptocurrency.ammountYouOwn ? cryptocurrency.ammountYouOwn : '' }
+								/>
+								<br />
+								<input style={{ width: '100%' }} type="submit" value="Submit" />
+							</form>
+						</td>
 						<td>{0.5 * cryptocurrency.quotes.USD.price}</td>
 					</tr>
 				))}
@@ -47,13 +58,32 @@ class CryptocurrenciesTableComponent extends React.Component {
 			.then(responseObject => this.setState({ responseObject, isLoading: false }));
 	}
 
+	handleChangeAmmountYouOwn = (event) => {
+		const target = event.target;
+		const value = target.value.trim();
+		const id = target.id;
+		let updatedDataRows = this.state.responseObject.data.map(cryptocurrency => {
+
+			if (cryptocurrency.id === Number(id)) {
+				return { ...cryptocurrency, ammountYouOwn: value };
+			}
+
+			return cryptocurrency;
+		});
+ 
+		let responseObjectUpdated = {...this.state.responseObject, data: updatedDataRows};
+
+		this.setState({...this.state, responseObject: responseObjectUpdated});
+
+	}
+
 	render() {
 		const isLoadingSuccessfullyData = !this.state.isLoading && this.state.responseObject;
 
 		return (
 			<div>
 				{!isLoadingSuccessfullyData ?
-					(<h2>Loading</h2 >) : (<ResultTable responseObject={this.state.responseObject} />)
+					(<h2>Loading</h2 >) : (<ResultTable responseObject={this.state.responseObject} handleChangeAmmountYouOwn={this.handleChangeAmmountYouOwn} />)
 				}
 			</div>
 		);
